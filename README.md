d21c90f041896db67ac741df7ca50c2792237caf
lauriely lourenço <49760702+laurielylourenco@users.noreply.github.com>
2023-05-28 18:17:10 -0300

    validar de formulario

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
+
 # LeadConnectorZap
+
 Usuário recebe uma mensagem no whastsapp quando ela se cadastra pelo formulário
+
+## Aprendizados
+
+Como começar um projeto usando React e como integrar ele com backend em Node.js.
+
+
+## Tarefas 
+- Frontend
+    - [ ] Validação do formulario usando Formkit
+    - [ ] Envio dados para API de cadastro
+- Backend
+    - [ ] Criar endpoint de cadastro dos dados
+
+## Stack utilizada
+
+**Front-end:** React
+
+**Back-end:** Node, Express
+
+
+## 🔗 Links
+
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/laurielylourenco/)
+
+
--- client-lead/src/App.js
+++ client-lead/src/App.js
@@ -1,19 +1,68 @@
 
 import './App.css';
+import { Formik, useField } from "formik";
+const Campo = props => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <input 
+      {...field} 
+      {...props} 
+      className={meta.error && meta.touched ? 'is-valid' : ''} 
+      />
+      { 
+        meta.error && meta.touched ?
+        (<div className='invalid-feedback'> {meta.error} </div>)
+        : null
+      }
+    </>
+  )
+}
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
       <div className="container">
-        <h1>Contact Us</h1>
-          <p>Cadastre-se é tenha mais informações!</p>
-        <form>
+        <h1>Entre em contato</h1>
+        <p>Cadastre-se é receba mais informações!</p>
 
-          <input type="text" placeholder="Nome" />
-          <input type="email" placeholder="E-mail" />
-          <input type="text" placeholder="Telefone" />
-          <button type="submit">Enviar</button>
-        </form>
+        <Formik initialValues={{ nome: "", email: "", telefone: "" }}
+          validate={(values) => {
+            const errors = {};
+            if (!values.nome) {
+              errors.nome = "O nome é obrigatorio"
+            }
+
+            if (!values.email) {
+              errors.email = "O email é obrigatorio";
+            } else if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i.test(values.email)) {
+
+              errors.email = "O formato de email incorreto";
+            }
+
+            if (!values.telefone) {
+              errors.telefone = "O telefone é obrigatorio"
+            }
+
+            return errors;
+          }}
+          onSubmit={(values) => {
+
+
+            alert(JSON.stringify(values))
+          }}
+        >
+          {(props) => (
+            <form onSubmit={props.handleSubmit} noValidate>
+
+              <Campo type="text"  id='nome' name='nome' placeholder="Nome" />
+              <Campo type="email" id='email' name="email"  placeholder="E-mail" />
+              <Campo type="text" id='telefone' name="telefone" placeholder="Telefone" />
+              <button type="submit">Enviar</button>
+            </form>
+          )}
+        </Formik>
       </div>
     </div>
   );
